5e4048c849f5797bacd2693c66561ab3eac08b63
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-04-06 15:40:39 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,27 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-console.log("test")
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('div.gallery');
+galleryEl.inneraHTML = markupGalleryItems(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function markupGalleryItems(listItems) {
+  return listItems
+    .map(
+      item => `
+          <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+          </a>
+        `
+    )
+    .join('');
+}
