00c9a62e72d62c250ad0aa015dd79b5f333cfc23
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-04-09 21:01:16 +0300

    add task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const DATA_TIME_STORAGE_KEY = 'videoplayer-current-time';
+const ifremRef = document.querySelector('#vimeo-player');
+const player = new Player(ifremRef);
+
+onResumeVideo();
+
+player.on('timeupdate', throttle(setDataTime, 1000));
+
+function setDataTime(data) {
+  localStorage.setItem(DATA_TIME_STORAGE_KEY, data.seconds);
+}
+
+function onResumeVideo() {
+  const currentPlayerTime = localStorage.getItem(DATA_TIME_STORAGE_KEY);
+
+  if (currentPlayerTime) {
+    player.setCurrentTime(currentPlayerTime);
+  }
+}
